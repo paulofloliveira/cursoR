8e0a6d5c8083bd5d86ed84efacf494d799127a3d
Paulo Fernando de Lima Oliveira <30387327+pflimaoliveira@users.noreply.github.com>
2017-08-16 16:50:14 -0300

    Add files via upload

---
+++ ss.R
@@ -0,0 +1,34 @@
+#Curso R - Semana de Métodos de Pesquisa#
+#---------------------------------------#
+3+5
+10-5
+92*1.6
+900/30
+40^2
+2==3
+2!=3
+"data"=="stats"
+"data"<"stats"
+x<-30
+x
+print(x)
+obj.1
+obj.1<-78
+obj.1
+obj.2<-1:20
+class(x1)
+x1<-c(TRUE,FALSE)
+x1<-c(T,F)
+X1
+x1
+class(x1)
+x4 <- c('Paulo','Larissa','Bia','Pedro')
+class(x4)
+x2 <- 9:29
+class(x2)
+x3 <- c(1.77,1.68,1.80,1.61)
+class(x3)
+data.frame (x4,x3)
+dados_altura <- data.frame(x4,x3,stringsAsFactors = FALSE)
+class(dados_altura)
+rm(obj.1,obj.2)
